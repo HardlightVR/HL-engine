636207ada4e050fb7cd8c309a199f473c0fe84d3
Casey Waldren <casey@nullspacevr.com>
2016-11-09 17:50:09 -0800

    Move instant into IPlayable

--- NS_Unreal_SDK/IPlayable.h
+++ NS_Unreal_SDK/IPlayable.h
@@ -12,3 +12,19 @@ public:
 	virtual void Update(float dt, PriorityModel& model) = 0;
 	virtual uint32_t GetHandle() const = 0;
 };
+
+
+template<typename T>
+class Instant {
+public:
+	T Item;
+	float Time;
+	bool Executed;
+	boost::uuids::uuid Handle;
+
+	std::function<bool(T i, float rhs)> comp;
+	Instant(float t, T i, std::function<bool(T i, float rhs)> c) :Time(t), Item(i), Executed(false), comp(c) {}
+	bool Expired() {
+		return comp(Item, Time);
+	}
+};
--- NS_Unreal_SDK/PlayableSequence.h
+++ NS_Unreal_SDK/PlayableSequence.h
@@ -2,46 +2,7 @@
 #include "IPlayable.h"
 #include "TimeInstant.h"
 
-#define ERROR_PARENTHESIS_MUST_BE_PLACED_AROUND_THE_RETURN_TYPE(...) __VA_ARGS__>::type
-#define FUNCTION_REQUIRES(...) typename boost::enable_if<boost::mpl::and_<__VA_ARGS__, boost::mpl::bool_<true> >, ERROR_PARENTHESIS_MUST_BE_PLACED_AROUND_THE_RETURN_TYPE
-#define EXCLUDE(...) typename boost::mpl::not_<typename boost::mpl::or_<__VA_ARGS__, boost::mpl::bool_<false> >::type >::type
 
-struct SequenceComparator
-{
-	bool operator()(JsonSequenceAtom atom, float atomTime)
-	{
-		return atomTime >= atom.Time;
-	}
-};
-
-template<typename T>
-class Instant {
-public:
-	T Item;
-	float Time;
-	bool Executed;
-	boost::uuids::uuid Handle;
-	
-	std::function<bool(T i, float rhs)> comp;
-	Instant(float t, T i, std::function<bool(T i, float rhs)> c) :Time(t), Item(i), Executed(false), comp(c) {}
-	bool Expired() {
-		return comp(Item, Time);
-	}
-};
-
-
-
-class MyInstant {
-public:
-	JsonSequenceAtom Item;
-	float Time;
-	bool Executed;
-	boost::uuids::uuid Handle;
-	MyInstant(float t, JsonSequenceAtom i) :Time(t), Item(i), Executed(false) {};
-	bool Expired() const {
-		return Time > Item.Time;
-	}
-};
 
 class PlayableSequence : public IPlayable {
 public:
