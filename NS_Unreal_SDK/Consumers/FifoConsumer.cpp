5627d7f6333d95643280d68d5191f92e67532dd4
Casey Waldren <casey@nullspacevr.com>
2016-12-15 10:39:04 -0800

    Add precompiled headers to a few more files

--- NS_Unreal_SDK/Consumers/FifoConsumer.cpp
+++ NS_Unreal_SDK/Consumers/FifoConsumer.cpp
@@ -1,3 +1,4 @@
+#include "StdAfx.h"
 #include "FifoConsumer.h"
 #include <iostream>
 
--- NS_Unreal_SDK/HapticDirectoryTools.cpp
+++ NS_Unreal_SDK/HapticDirectoryTools.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "HapticDirectoryTools.h"
 #include <boost/filesystem.hpp>
 #include <iostream>
