de3dda3e9a8b73cb60b3aeca18db408977cdd459
Casey Waldren <casey@nullspacevr.com>
2017-04-28 11:28:46 -0700

    Make build script more flexible
    Same usage: .\Build.ps1 [-debug/-release]. You can now add projects and the dlls that they create. No name transformations yet.

--- NS_Unreal_SDK/Build.ps1
+++ NS_Unreal_SDK/Build.ps1
@@ -1,71 +1,117 @@
-param (
+Param(
 	[switch]$debug,
 	[switch]$release
 )
 
-$loader_dll = 'NSLoader.dll'
-$loader_root_dir = 'C:\Users\NullSpace Team\Documents\Visual Studio 2015\Projects\NSLoader'
-$loader_x86_dir = $loader_root_dir + '\'
-$loader_x64_dir = $loader_root_dir + '\x64\'
-
-Write-Host "For Loader DLL:" `n "x86:"  $loader_x86_dir `n "x64: " $loader_x64_dir `n
-
-$wrapper_dll = 'NSLoaderWrapper.dll'
-$wrapper_root_dir = 'C:\Users\NullSpace Team\Documents\Visual Studio 2015\Projects\NSLoaderWrapper\NSLoaderWrapper'
-$wrapper_x86_dir = $wrapper_root_dir + '\bin\x86\'
-$wrapper_x64_dir = $wrapper_root_dir + '\bin\x64\'
-
-
-Write-Host "For Wrapper DLL:" `n "x86:"  $wrapper_x86_dir `n "x64: " $wrapper_x64_dir `n
-
-
-$API_root_dir = 'C:\Users\NullSpace Team\Documents\NullSpace SDK 0.1.1\Assets\NullSpace SDK\Plugins'
-$API_x86_dir = $API_root_dir + '\x86\'
-$API_x64_dir = $API_root_dir + '\x86_64\'
-
-Write-Host "For Unity PLugin Folder:" `n "x86:"  $API_x86_dir `n "x64: " $API_x64_dir `n
-
-
-function copy_debug_to_wrapper {
-	Copy-Item -Path $loader_x86_dir"Debug\"$loader_dll -Destination $wrapper_x86_dir"Debug\"
-	Copy-Item -Path $loader_x64_dir"Debug\"$loader_dll -Destination $wrapper_x64_dir"Debug\"
+ $projects = @{
+ 	"plugin" = "C:\Users\NullSpace Team\Documents\Visual Studio 2015\Projects\NSLoader\build\bin";
+ 	"wrapper" = "C:\Users\NullSpace Team\Documents\Visual Studio 2015\Projects\NSLoaderWrapper\NSLoaderWrapper\bin";
+ 	"unity" = "C:\Users\NullSpace Team\Documents\NullSpace SDK 0.1.1\Assets\NullSpace SDK\Plugins";
+ }
+$project_dll_names = @{
+	"plugin" = "NSLoader.dll";
+	"wrapper" = "NSLoaderWrapper.dll", "NSLoader.dll"
+	"unity" = "NSLoader.dll", "NSLoaderWrapper.dll"
 }
 
-function copy_release_to_wrapper {
-	Copy-Item -Path $loader_x86_dir"Release\"$loader_dll -Destination $wrapper_x86_dir"Release\"
-	Copy-Item -Path $loader_x64_dir"Release\"$loader_dll -Destination $wrapper_x64_dir"Release\"
 
-}
-function copy_debug_to_unity {
-	Copy-Item -Path $wrapper_x86_dir"Debug\"$wrapper_dll -Destination $API_x86_dir
-	Copy-Item -Path $wrapper_x86_dir"Debug\"$loader_dll -Destination $API_x86_dir
+ $projects_build_dirs = @{
+ 	"plugin" = @{
+ 		"debug" = @{
+ 			"win32" = "Debug\Win32";
+ 			"win64" = "Debug\Win64";
+ 		};
+ 		"release" = @{
+ 			"win32" = "Release\Win32";
+ 			"win64" = "Release\Win64";
+ 		};
+ 	};
+ 	"wrapper" = @{
+ 		"debug" = @{
+ 			"win32" = "x86\Debug";
+ 			"win64" = "x64\Debug";
+ 		};
+ 		"release" = @{
+ 			"win32" = "x86\Release";
+ 			"win64" = "x64\Release";
+ 		};
+ 	};
+ 	"unity" = @{
+ 		"debug" = @{
+ 			"win32" = "x86";
+ 			"win64" = "x86_64";
+ 		};
+ 		"release" = @{
+ 			"win32" = "x86";
+ 			"win64" = "x86_64";
+ 		};
+ 	};
 
-	Copy-Item -Path $wrapper_x64_dir"Debug\"$wrapper_dll -Destination $API_x64_dir
-	Copy-Item -Path $wrapper_x64_dir"Debug\"$loader_dll -Destination $API_x64_dir
 
-}
+ }
 
-function copy_release_to_unity {
-	Copy-Item -Path $wrapper_x86_dir"Release\"$wrapper_dll -Destination $API_x86_dir
-	Copy-Item -Path $wrapper_x86_dir"Release\"$loader_dll -Destination $API_x86_dir
 
-	Copy-Item -Path $wrapper_x64_dir"Release\"$wrapper_dll -Destination $API_x64_dir
-	Copy-Item -Path $wrapper_x64_dir"Release\"$loader_dll -Destination $API_x64_dir
+function copy_dlls_from_to($projectA, $projectB, $configuration) {
+
+
+	$B_root_path = $projects[$projectB];
+	$A_root_path = $projects[$projectA];
+
+
+
+	$A_win32_dir = Join-Path $A_root_path $projects_build_dirs[$projectA][$configuration]["win32"];
+	$A_win64_dir = Join-Path $A_root_path $projects_build_dirs[$projectA][$configuration]["win64"];
+	
+	$B_win32_dir = Join-Path $B_root_path $projects_build_dirs[$projectB][$configuration]["win32"];
+	$B_win64_dir = Join-Path $B_root_path $projects_build_dirs[$projectB][$configuration]["win64"];
+
+	#Write-Host "$($projectA) root path: $($A_root_path)";
+	#Write-Host "$($projectA) win32: $($A_win32_dir)";
+	#Write-Host "$($projectA) win64: $($A_win64_dir)";
+
+	
+#	Write-Host "$($projectB) root path: $($B_root_path)";
+#	Write-Host "$($projectB) win32: $($B_win32_dir)";
+#	Write-Host "$($projectB) win64: $($B_win64_dir)";
+
+	Foreach ($dll in $project_dll_names[$projectA]) {
+
+		$a_win32_dll = Join-Path $A_win32_dir $dll;
+		$b_win32_dll = Join-Path $B_win32_dir $dll;
+
+		$a_win64_dll = Join-Path $A_win64_dir $dll;
+		$b_win64_dll = Join-Path $B_win64_dir $dll;
+
+		Write-Host "Copying $($a_win32_dll) to $($b_win32_dll)";
+
+		Write-Host "Copying $($a_win64_dll) to $($b_win64_dll)";
+		Copy-Item -Path $a_win32_dll -Destination $b_win32_dll
+		Copy-Item -Path $a_win64_dll -Destination $b_win64_dll
+
+	}
+
+
 
 }
 
 if ($debug) {
-	Write-Host "Copying from loader to wrapper.."
-	copy_debug_to_wrapper
-	Write-Host "Copying from wrapper to unity.."
-	copy_debug_to_unity
-	Write-Host "Done!"
+
+	copy_dlls_from_to "plugin" "wrapper" "debug"
+	copy_dlls_from_to "wrapper" "unity" "debug"
+	#Write-Host "Copying from loader to wrapper.."
+	#copy_debug_to_wrapper
+	#Write-Host "Copying from wrapper to unity.."
+	#copy_debug_to_unity
+	#Write-Host "Done!"
 }
 
 if ($release) {
-	Write-Host "Copying from loader to wrapper.."
-	copy_release_to_wrapper
-	Write-Host "Copying from wrapper to unity.."
-	copy_release_to_unity
-	Write-Host "Done!"
+
+	copy_dlls_from_to "plugin" "wrapper" "release"
+	copy_dlls_from_to "wrapper" "unity" "release"
+	#Write-Host "Copying from loader to wrapper.."
+	#copy_release_to_wrapper
+	#Write-Host "Copying from wrapper to unity.."
+	#copy_release_to_unity
+	#Write-Host "Done!"
 }
