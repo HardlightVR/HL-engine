003303a8185c0dc0838bb593447623abbe0f43e8
Casey Waldren <casey@nullspacevr.com>
2017-05-01 15:16:06 -0700

    Add documentation and tagging to build tool

--- src/BuildTools/test.ps1
+++ src/BuildTools/test.ps1
@@ -1,24 +1,21 @@
 Add-Type -AssemblyName System.Windows.Forms 
 
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-Import-Module -Name "C:\Users\NullSpace Team\Documents\NS_Unreal_SDK\NS_Unreal_SDK\Invoke-MsBuild.psm1"
+
+# Welcome to the NullSpace VR build tool. Hopefully this is temporary. But who knows how long it will last?
 
 
 
-$registryPath = "HKCU:\SOFTWARE\Microsoft\VisualStudio\14.0_Config\MSBuild"
-$name = "EnableOutOfProcBuild"
-$value = 0
+# If you need to add MS build because of a build step, go download the Invoke-MsBuild script and include it like so
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+# Import-Module -Name "C:\Users\NullSpace Team\Documents\NS_Unreal_SDK\NS_Unreal_SDK\Invoke-MsBuild.psm1"
 
-if(!(Test-Path $registryPath)) {
-    New-Item -Path $registryPath -Force | Out-Null
-    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
-}
-else {
-    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
-}
 
-$devenv = "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe"
- $repo_directories = @{
+
+# The location of devenv, which we use for building
+$DEVENV = "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe"
+ 
+ # Table of our different project git repo locations. This script assumes that the projects are git repos.
+$repo_directories = @{
         # for the Unity SDK, .unitypackage
         "unity_sdk" = "$Env:USERPROFILE\Documents\NullSpace SDK 0.1.1";
 
@@ -45,10 +42,11 @@ $devenv = "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/deven
 
         # The UE plugin
         "unreal_plugin" = "D:\UnrealEngine-release\Engine\Plugins\Runtime\HapticSuit";
-
-
     }
 
+
+# When we want to tag something with git, we want a friendlier name than what's in the table above
+# Only need to fill out what's necessary
 $tag_friendly_names = @{
 
     "unity_sdk" = "Unity_SDK";
@@ -56,15 +54,32 @@ $tag_friendly_names = @{
     "plugin" = "Plugin";
     "installer" = "Service";
     "asset_tool" = "AssetTool";
+    "unreal_plugin" = "UnrealPlugin"
 }
 
+# Group together some repos to make a product. This means they will be tagged with a common release number when building.
 $products =    @{
 
-    "chimera" = "unity_sdk", "installer", "asset_tool";
+    "chimera" = "unity_sdk", "installer", "asset_tool"; 
     "installer" = @("installer");
 
 }
 
+# We need to edit the registry in order for external builds to work
+function do_registry_hack() {
+    $registryPath = "HKCU:\SOFTWARE\Microsoft\VisualStudio\14.0_Config\MSBuild"
+    $name = "EnableOutOfProcBuild"
+    $value = 0
+    if(!(Test-Path $registryPath)) {
+        New-Item -Path $registryPath -Force | Out-Null
+        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+    }
+    else {
+        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+    }
+}
+
+# Generate the little header that appears above release notes. Useful for figuring out what internal versions correspond to a marketing release. 
 function generate_chimera_version_header($chimera_version, $service, $plugin, $unity, $asset_tool) {
     $output_str = "Chimera SDK $chimera_version`n"
     $output_str += "------------------------`n"
@@ -75,6 +90,7 @@ function generate_chimera_version_header($chimera_version, $service, $plugin, $u
     $output_str += "`n"
     return $output_str;
 }
+
 # Copy all files in $file_list from the directory $source to the directory $destination
 function copy_all($source, $file_list, $destination)
 {
@@ -85,7 +101,7 @@ function copy_all($source, $file_list, $destination)
     }
 }
 
-# copy the directories in $dir_list recursively into $destination 
+# Copy the directories in $dir_list recursively into $destination 
 function copy_all_dirs($source, $dir_list, $destination) {
     Foreach ($dir in $dir_list) {
         $source_path = Join-Path $source $dir
@@ -93,24 +109,36 @@ function copy_all_dirs($source, $dir_list, $destination) {
     }
 }
 
-
-function tag([String] $git_repo, [string]$tag, [string]$tag_message) { 
+# Tags a git repo with string $tag and message $tag_msg
+# Attempts to correct some simple mistakes e.g. including a ':' or '*' (replaces with '_')
+function tag([String] $git_repo, [string]$tag, [string]$tag_msg) { 
 
     #some common things that aren't allowed in tags. Not the full list. See here
     #http://stackoverflow.com/questions/26382234/what-names-are-valid-git-tags
     $tag = ($tag -replace "\s|~|:|\^|\?|\*", '_')
 
     Push-Location -Path $git_repo
-    if (-not $tag_msg) {
+    if ([string]::IsNullOrEmpty($tag_msg)) {
         $tag_msg = "(No message)"
     }  
-    $cmd = "git tag -a `"$tag`" -m `"$tag_message`""
 
-   Invoke-Expression $cmd 
 
+    $cmd = "git tag -a `"$tag`" -m `"$tag_msg`""
+    Invoke-Expression $cmd 
     Pop-Location
 
 }
+
+
+####################
+# Each product has a make_x function. It is responsible for making that project and pulling in dependencies, for some definition of make. 
+# Right now it's mostly copying build results
+# Convention: $essential_files contains a list of the essential files in the product
+#             $essential_folders contains a list of the essential folder(s) necessary. If I include "whatever", then I want ALL the files in "whatever". 
+####################
+
+
+
 
 $make_diagnostics_tool = { 
     param([HashTable]$dirs, [String] $destination, [HashTable] $options) 
@@ -121,7 +149,9 @@ $make_diagnostics_tool = {
 
 }
 
-
+# More complex build step
+# This function bumps the version numbers for chimera and the service, then rebuilds the project. 
+# Finally, it copies the output like all the other make_x's 
 $make_installer = {
     param([HashTable]$dirs, [String] $destination, [HashTable] $options) 
 
@@ -129,45 +159,55 @@ $make_installer = {
     $new_service_version = $options["service_version_bump"];
     $new_chimera_version = $options["chimera_version_bump"];
 
-
+    # Load up the resx XML from the project
     $resx_file_path = (Join-Path $dirs["installer"] "NSVRGui\Properties\Resources.resx")
     [xml] $resx = Get-Content $resx_file_path
 
+    # If we fail to read it, then we will need the user to do the version bump themselves.
     if ($resx.root.data -eq $null) {
-        Write-Host "Failed to read xml from the service gui's resx! Cannot bump versions automatically. You'll have to do it manually."
-        Read-Host "Hit enter once you've done it [enter]";
+        Write-Host "Failed to read xml from the service gui's resx! Cannot bump versions automatically."
+        Write-Host "Open up the GUI project, and navigate to the resources file. Use the helpful GUI to change the version strings."
+        Write-Host "    Service version: >> $new_service_version <<"
+        Write-Host "    Chimera version: >> $new_chimera_version <<"
+        Read-Host "Hit enter once that's done [enter]";
     } else {
-        $service_version = ($resx.root.data|where {$_.name -eq "ServiceVersion"})
-        $distribution_version = ($resx.root.data|where {$_.name -eq "ChimeraVersion"})
 
-        Write-Host "Bumping service $($service_version.value) --> $($new_service_version)"
+        # Read the previous versions 
+        $old_service_version = ($resx.root.data|where {$_.name -eq "ServiceVersion"})
+        $old_chimera_version = ($resx.root.data|where {$_.name -eq "ChimeraVersion"})
 
-        $service_version.Value = $new_service_version;
+        Write-Host "Bumping service $($old_service_version.value) --> $($new_service_version)"
 
-        Write-Host "Bumping chimera $($distribution_version.value) --> $($new_chimera_version)"
+        $old_service_version.Value = $new_service_version;
 
-        $distribution_version.Value = $new_chimera_version;
+        Write-Host "Bumping chimera $($old_chimera_version.value) --> $($new_chimera_version)"
+
+        $old_chimera_version.Value = $new_chimera_version;
 
         $resx.Save($resx_file_path)
-        
-        
     }
+
+
 
     $sln = (Join-Path $dirs["installer"] "NSVRService.sln") 
       
-    Write-Host "Rebuilding the installer, just for you.."
-    & "$devenv" $sln /Rebuild Release /project (Join-Path $dirs["installer"] "NSVRServiceSetup\NSVRServiceSetup.vdproj") | Out-Null
+    Write-Host "Rebuilding the installer.."
+    & "$DEVENV" $sln /Rebuild Release /project (Join-Path $dirs["installer"] "NSVRServiceSetup\NSVRServiceSetup.vdproj") | Out-Null
     Write-Host "Finished."
+
+    # Now we can finally copy everything!
 
     $essential_files = "setup.exe", "NSVRServiceSetup.msi"
     $essential_folders = "vcredist_x86", "vcredist_x64", "DotNetFX45"
 
     $build_path = Join-Path $dirs["installer"] "NSVRServiceSetup\Release"
 
+    Copy-Item -Path (Join-Path $dirs["installer"] "release_notes.txt") -Destination $destination
     copy_all $build_path $essential_files $destination
     copy_all_dirs $build_path $essential_folders $destination
 
 }
+
 
 $make_unitypackage= {
     param([HashTable]$dirs, [String] $destination, [HashTable] $options) 
@@ -177,7 +217,7 @@ $make_unitypackage= {
     }
     $unity_package_name = "$($unity_package_name).unitypackage"
     if (-not (Test-Path (Join-Path $repo_directories["unity_sdk"] $unity_package_name))) {
-        Write-Host "That package doesn't seem to exist"
+        Write-Host "That package doesn't seem to exist. Not copying in the unitypackage."
     }
 
     $essential_files = @($unity_package_name)
@@ -190,7 +230,9 @@ $make_assettool = {
     param([HashTable]$dirs, [String] $destination, [HashTable] $options) 
 
     $essential_files = @("HapticAssetTools.exe")
-    copy_all $repo_directories["asset_tool"] $essential_files $destination
+    $release_dir = Join-Path $repo_directories["asset_tool"] "Release"
+Write-Host "Release dir: $release_dir"
+    copy_all $release_dir $essential_files $destination
 
 }
 $build_functions = @{
@@ -200,10 +242,12 @@ $build_functions = @{
     "asset_tool" = $make_assettool;
 }
 
+# Given a version tage SomeProduct_v0.1.3, return 0.1.3
 function number_from_version_tag([string]$tag) {
     $index_of_v = $tag.IndexOf("_v")
     return $tag.Substring($index_of_v+1)
 }
+
 
 function get_git_branch([string] $repo) {
     Push-Location $repo_directories[$repo]
@@ -211,29 +255,42 @@ function get_git_branch([string] $repo) {
     Pop-Location
     return $branch;
 }
-function latest_tagged_git_release([string]$repo, [string] $prefix) {
+
+# Given a bunch of tags like v0.1.2, v0.1.3, return the latest, i.e. v0.1.3 in this case
+function latest_tag_name([string]$repo, [string] $prefix) {
+
 
    Push-Location $repo_directories[$repo]
   
-  
-    $commit = iex "git rev-parse HEAD"
     $cmd = "git config --global versionsort.prereleaseSuffix `"-rc`""
     iex $cmd
     $cmd = "git tag -l --sort=-version:refname `"$($prefix)_v*`""
     $result = Invoke-Expression $cmd
     if (-not $result) {
         Pop-Location
-        return "(No version tag found) ($commit)"
+        return "(No version tag found)"
     }
     $latest_tag = $result.Split(' ')[0]
 
-    
-    $latest_tag = "$latest_tag ($commit)"
     Pop-Location
 
     return $latest_tag
 }
 
+function latest_tag_commit([string] $repo, [string] $prefix) {
+     Push-Location $repo_directories[$repo]
+  
+  
+    $commit = iex "git rev-parse HEAD"
+    Pop-Location
+    if (-not $commit) {
+        return "(no commit found?!)"
+    } else {
+        return $commit
+    }
+}
+
+# Wrapper around a make_x function. Handles tagging and creating the output directory.
 function do_build([String] $component_name, [HashTable] $options, [String] $path) {
 
     Write-Host "------- Packaging $component_name -------"
@@ -254,27 +311,53 @@ function do_build([String] $component_name, [HashTable] $options, [String] $path
 $chimera_wizard = {
     param([HashTable] $options)
 
-    $chimera_version = Read-Host "You're planning a new Chimera release. What shall be the marketing version number?"
+    [string] $chimera_version = Read-Host "You're planning a new Chimera release. What shall be the marketing version number?"
 
     Write-Host "Great, version >>>>> $chimera_version <<<<<`n"
 
     Write-Host "This release will contain the following products:`n"
     Foreach ($repo in $products["chimera"]) {
         $branch = get_git_branch $repo
-        $release = "[$branch] $(latest_tagged_git_release $repo $tag_friendly_names[$repo])"
+        $release = "[$branch] $(latest_tag_name $repo $tag_friendly_names[$repo]) ($(latest_tag_commit $repo $tag_friendly_names[$repo]))"
         Write-Host "$repo = $release"
       #  Write-Host (number_from_version_tag $release)
     }
 
+    $location = "C:\Users\NullSpace Team\Documents\Visual Studio 2015\Projects\DiagnosticsTool\Release\TestBuild"
 
     $result = Read-Host "Does this configuration look correct? [y/n]"
     if ($result -contains "y") {
-        do_build "installer" $options "C:\Users\NullSpace Team\Documents\Visual Studio 2015\Projects\DiagnosticsTool\Release\TestBuild"
-        do_build "unitypackage" $options "C:\Users\NullSpace Team\Documents\Visual Studio 2015\Projects\DiagnosticsTool\Release\TestBuild"
-        do_build "asset_tool" $options "C:\Users\NullSpace Team\Documents\Visual Studio 2015\Projects\DiagnosticsTool\Release\TestBuild"
+        $service_version =  number_from_version_tag (latest_tag_name "installer" $tag_friendly_names["installer"])
+        $plugin_version = number_from_version_tag (latest_tag_name "plugin" $tag_friendly_names["plugin"])
+        $unity_version = number_from_version_tag (latest_tag_name "unity_sdk" $tag_friendly_names["unity_sdk"])
+        $assettool_version = number_from_version_tag (latest_tag_name "asset_tool" $tag_friendly_names["asset_tool"])
 
+
+        $options["chimera_version_bump"] = $chimera_version;
+        $options["service_version_bump"] = $service_version
+
+        # we don't do_build the asset tool because it is pulled in by the installer
+        # do_build "asset_tool" $options $location 
+        # perhaps do_build is bad name, but it is unclear if we should have this tool building stuff or just packaging for now.
+        do_build "installer" $options $location
+        do_build "unitypackage" $options $location
+
+
+        $header_text = generate_chimera_version_header $chimera_version $service_version $plugin_version $unity_version $assettool_version
+        New-Item (Join-Path $location "versions.txt") -type file -force -value $header_text | Out-Null
+
+        Write-Host "`nFinished packaging chimera at location $location"
+        $tag_all = Read-Host "Want to tag everything with the chimera release number? [y/n]"
+        if ($tag_all -contains "y") {
+             Foreach ($repo in $products["chimera"])  {
+                $dir = $repo_directories[$repo]
+                tag $dir "Chimera_v$($chimera_version)"
+             }
+        }
 
     } 
+
+
 
 }
 $product_wizards = @{
@@ -283,6 +366,7 @@ $product_wizards = @{
 }
 
 function Main(){
+    do_registry_hack
 
     $options = @{
         "tag_repo" = "TestTag";
