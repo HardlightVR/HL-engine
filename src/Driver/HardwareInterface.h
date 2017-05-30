455411f9022f1a494dc9d7848e81ead6e37579b3
Casey Waldren <casey@nullspacevr.com>
2017-05-29 17:41:37 -0700

    Change range for RTP to 0-128
    Because 128-255 is the motor spinning backwards

--- src/Driver/HardwareInterface.h
+++ src/Driver/HardwareInterface.h
@@ -26,7 +26,7 @@ public:
 			m_firmware.PlayEffectContinuous(Location(ec.area()), ec.effect(), ec.strength());
 		}
 		else if (ec.command() == NullSpaceIPC::EffectCommand_Command_PLAY_RTP) {
-			int strength = int(ec.strength() * 255.0f);
+			int strength = int(ec.strength() * 128.0f);
 			m_firmware.PlayRtp(Location(ec.area()), strength);
 		}
 		else if (ec.command() == NullSpaceIPC::EffectCommand_Command_ENABLE_RTP) {
