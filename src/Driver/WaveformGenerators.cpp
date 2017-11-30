6a76cbb383ebc3bae1a819ad23c891c86bd03d91
Casey Waldren <caseywaldren@gmail.com>
2017-11-30 15:00:27 -0800

    Refactor WaveformGenerator to be more functional

--- src/Driver/WaveformGenerators.cpp
+++ src/Driver/WaveformGenerators.cpp
@@ -4,6 +4,40 @@
 namespace nsvr {
 namespace waveforms {
 
+void append(std::vector<double>& samples, const std::vector<double>& newSamples) {
+
+	samples.insert(samples.end(), newSamples.begin(), newSamples.end());
+
+}
+
+std::vector<double> sin_sample(float strength, std::size_t numSamples) {
+	float clampedStrength = std::max(0.0f, std::min(1.0f, strength));
+	std::vector<double> samples;
+	samples.reserve(numSamples);
+
+	float total = static_cast<float>(M_PI / numSamples);
+	for (std::size_t i = 0; i < numSamples; i++) {
+
+		float s = std::sin((float)i*total)*clampedStrength;
+		if (s > 0) {
+			samples.push_back(s);
+		}
+		else {
+			samples.push_back(0);
+		}
+	}
+	return samples;
+}
+
+std::vector<double> repeated(float strength, std::size_t numSamples) {
+	std::vector<double> samples;
+	samples.reserve(numSamples);
+	float clampedStrength = std::max(0.0f, std::min(1.0f, strength));
+	for (std::size_t i = 0; i < numSamples; i++) {
+		samples.push_back(clampedStrength);
+	}
+	return samples;
+}
 
 void sinsample(std::vector<double>& samples, float maxStrength, std::size_t numSamples) {
 	float clampedStrength = std::max(0.0f, std::min(1.0f, maxStrength));
@@ -38,15 +72,12 @@ std::vector<double> generateWaveform(float strength, nsvr_default_waveform famil
 		std::vector<double> samples;
 		const int ten_percent = how_many * .1;
 		const int ninety_percent = how_many * .9;
-		sinsample(samples, strength, ninety_percent);
-		constant(samples, 0, ten_percent);
-
+		append(samples, sin_sample(strength, ninety_percent));
+		append(samples, repeated(0, ten_percent));
 		return samples;
 	}
 	else if (nsvr_preset_family_tick == family) {
-		std::vector<double> samples;
-		constant(samples, strength, 5);
-		return samples;
+		return repeated(strength, 5);
 	}
 	else if (nsvr_preset_family_buzz == family) {
 
@@ -54,8 +85,8 @@ std::vector<double> generateWaveform(float strength, nsvr_default_waveform famil
 		const int six_percent = how_many * .06;
 		std::vector<double> samples;
 		for (int i = 0; i < 6; i++) {
-			constant(samples, strength, ten_percent);
-			constant(samples, 0, six_percent);
+			append(samples, repeated(strength, ten_percent));
+			append(samples, repeated(0, six_percent));
 		}
 
 		return samples;
@@ -65,11 +96,11 @@ std::vector<double> generateWaveform(float strength, nsvr_default_waveform famil
 		const int thirteen_percent = how_many * 0.13;
 		const int fortytwo_percent = how_many * 0.42;
 		const int thirtytwo_percent = how_many * 0.32;
-		sinsample(samples, strength, thirteen_percent);
-		constant(samples, 0.0f, fortytwo_percent);
-		sinsample(samples, strength, thirteen_percent);
-		constant(samples, 0.0f, thirtytwo_percent);
 
+		append(samples, sin_sample(strength, thirteen_percent));
+		append(samples, repeated(0, fortytwo_percent));
+		append(samples, sin_sample(strength, thirteen_percent));
+		append(samples, repeated(0, thirtytwo_percent));
 		return samples;
 	}
 	else if (nsvr_preset_family_triple_click == family) {
@@ -78,21 +109,17 @@ std::vector<double> generateWaveform(float strength, nsvr_default_waveform famil
 		const int one_quarter = one_third * 0.25;
 		const int three_quarter = one_third * 0.75;
 		for (int i = 0; i < 3; i++) {
-			sinsample(samples, strength, one_quarter);
-			constant(samples, 0.0f, three_quarter);
+			append(samples, sin_sample(strength, one_quarter));
+			append(samples, repeated(0, three_quarter));
 		}
 		return samples;
 	}
 	else if (nsvr_preset_family_hum == family) {
-		std::vector<double> samples;
-		constant(samples, strength, how_many);
-		return samples;
+		return repeated(strength, how_many);
 	}
 
 	else {
-		std::vector<double> samples;
-		constant(samples, strength, how_many);
-		return samples;
+		return repeated(strength, how_many);
 	}
 
 }
--- src/Driver/WaveformGenerators.h
+++ src/Driver/WaveformGenerators.h
@@ -11,8 +11,14 @@ namespace nsvr {
 namespace waveforms {
 
 
+	std::vector<double> sin_sample(float strength, std::size_t numSamples);
+	std::vector<double> repeated(float strength, std::size_t numSamples);
+
 	void sinsample(std::vector<double>& samples, float maxStrength, std::size_t numSamples);
 	void constant(std::vector<double>& samples, float strength, std::size_t numsamples);
 	std::vector<double> generateWaveform(float strength, nsvr_default_waveform family, int how_many);
+
+	void append(std::vector<double>& samples, const std::vector<double>& newSamples);
+
 
 }}
