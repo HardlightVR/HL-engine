20fe03926dfb4bb3c03440eb5d35a0a140a714e3
Casey Waldren <caseywaldren@gmail.com>
2017-12-07 16:25:43 -0800

    Remove dead code

--- src/Driver/WaveformGenerators.cpp
+++ src/Driver/WaveformGenerators.cpp
@@ -39,34 +39,10 @@ std::vector<double> repeated(float strength, std::size_t numSamples) {
 	return samples;
 }
 
-void sinsample(std::vector<double>& samples, float maxStrength, std::size_t numSamples) {
-	float clampedStrength = std::max(0.0f, std::min(1.0f, maxStrength));
 
-	float total = static_cast<float>(M_PI / numSamples);
-	for (std::size_t i = 0; i < numSamples; i++) {
-
-		float s = std::sin((float)i*total)*clampedStrength;
-		if (s > 0) {
-			samples.push_back(s);
-		}
-		else {
-			samples.push_back(0);
-		}
-	}
-
-}
-
-void constant(std::vector<double>& samples, float strength, std::size_t numsamples) {
-	float clampedStrength = std::max(0.0f, std::min(1.0f, strength));
-	for (std::size_t i = 0; i < numsamples; i++) {
-		samples.push_back(clampedStrength);
-	}
-}
 std::vector<double> generateWaveform(float strength, nsvr_default_waveform family, int how_many) {
 	if (nsvr_preset_family_click == family) {
-		std::vector<double> samples;
-		sinsample(samples, strength, how_many);
-		return samples;
+		return sin_sample(strength, how_many);
 	}
 	else if (nsvr_preset_family_pulse == family) {
 		std::vector<double> samples;
